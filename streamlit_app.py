298fba7b26c0d23fda72ebfd8cc41a2b24509789
Joseph Pollack <Tonic@users.noreply.huggingface.co>
2023-10-15 22:04:40 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,10 @@ audio = audiorecorder("Click to record audio", "Click to stop recording")
 
 if len(audio) > 0:
     with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as audio_file:
-        audio.export().save(audio_file.name)
+        audio_file.write(audio.export())
+        audio_file_path = audio_file.name
 
-    st.audio(audio_file.name, format="audio/wav")  # Show audio only if it's recorded
+    st.audio(audio_file_path, format="audio/wav")  # Show audio only if it's recorded
 
     submit_button = st.button("Use this audio")
 
@@ -32,7 +33,7 @@ if len(audio) > 0:
         # Load the Whisper model
         model = whisper.load_model("base")
 
-        result = model.transcribe(audio_file.name)
+        result = model.transcribe(audio_file_path)
         st.info("Transcribing...")
 
         st.success("Transcription complete")
